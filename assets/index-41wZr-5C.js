12e02740bfa038b497ac26b31c5a7cfc35d710c4
llkui241569 <llkui241569@163.com>
2024-01-28 21:08:08 +0800

    gh-pages release v0.0.1-alpha.6

--- assets/index-41wZr-5C.js
+++ assets/index-41wZr-5C.js
@@ -82,7 +82,7 @@ $ yarn add pelement-react\r
 $ pnpm install pelement-react
 `})})]})}function _0(t={}){const{wrapper:e}=t.components||{};return e?n.jsx(e,{...t,children:n.jsx(Du,{...t})}):Du(t)}function Fu(t){const e={h1:"h1",h3:"h3",h4:"h4",hr:"hr",li:"li",p:"p",ul:"ul",...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{children:"更新日志"}),`
 `,n.jsx(e.h3,{children:"0.0.1-alpha.6"}),`
-`,n.jsx(e.p,{children:"be pending"}),`
+`,n.jsx(e.p,{children:"2024-01-28"}),`
 `,n.jsx(e.h4,{children:"Features"}),`
 `,n.jsxs(e.ul,{children:[`
 `,n.jsx(e.li,{children:"Components add Popconfirm"}),`

5506848b3c67d647d118ff1eb5ab980f50a42bbc
llkui241569 <llkui241569@163.com>
2024-04-28 23:16:09 +0800

    gh-pages chore: bump react from 18.2.0 to 18.3.1

---
+++ assets/index.zh-CN-D_0fntvU.js
@@ -0,0 +1,6 @@
+import{j as n}from"./index-DGvRp03S.js";import{C as o}from"./CodeBlock-BTG5cIIT.js";import{n as t}from"./index-T792z6nM.js";function s(){return n.jsx(n.Fragment,{children:n.jsx(t,{height:"400px",children:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map((r,e)=>n.jsx("p",{className:"scrollbar-demo-item",children:r},e))})})}function c(r){const e={code:"code",h1:"h1",h2:"h2",p:"p",...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{children:"Scrollbar 滚动条"}),`
+`,`
+`,n.jsx(e.p,{children:"用于替换浏览器原生滚动条。"}),`
+`,n.jsx(e.h2,{children:"基础用法"}),`
+`,n.jsxs(e.p,{children:["通过 ",n.jsx(e.code,{children:"height"})," 属性设置滚动条高度，若不设置则根据父容器高度自适应。"]}),`
+`,n.jsx(o,{name:"ScrollbarBasic",children:n.jsx(s,{})})]})}function h(r={}){const{wrapper:e}=r.components||{};return e?n.jsx(e,{...r,children:n.jsx(c,{...r})}):c(r)}export{h as default};

2505eb221647ff4bfb6fbada7e9c7bb94eba42e4
llkui241569 <llkui241569@163.com>
2024-04-30 07:52:03 +0800

    gh-pages feat: add Message

---
+++ assets/index.zh-CN-CpKImw0W.js
@@ -0,0 +1,17 @@
+import{j as e}from"./index-BRgxcaXn.js";import{C as t}from"./CodeBlock-CGFaTMI6.js";import{p as s,e as d}from"./index-GUx837jA.js";function h(){return e.jsx(e.Fragment,{children:e.jsxs("div",{children:[e.jsx("span",{children:"I sit at my window this morning where the world like a passer-by stops for a moment, nods to me and goes."}),e.jsx(s,{}),e.jsx("span",{children:"There little thoughts are the rustle of leaves; they have their whisper of joy in my mind."})]})})}function l(){return e.jsx(e.Fragment,{children:e.jsxs("div",{children:[e.jsx("span",{children:"What you are you do not see, what you see is your shadow. "}),e.jsx(s,{contentPosition:"left",children:"Rabindranath Tagore"}),e.jsx("span",{children:"My wishes are fools, they shout across thy song, my Master. Let me but listen."}),e.jsx(s,{children:e.jsx(d,{})}),e.jsx("span",{children:"I cannot choose the best. The best chooses me."}),e.jsx(s,{contentPosition:"right",children:"Rabindranath Tagore"})]})})}function o(){return e.jsxs(e.Fragment,{children:[e.jsxs("div",{children:[e.jsx("span",{children:"What language is thine, O sea?"}),e.jsx(s,{borderStyle:"dashed"}),e.jsx("span",{children:"The language of eternal question."})]}),e.jsx(s,{borderStyle:"dotted"}),e.jsx("span",{children:"What language is thy answer, O sky?"}),e.jsx(s,{borderStyle:"double"}),e.jsx("span",{children:"The language of eternal silence."})]})}function c(){return e.jsx(e.Fragment,{children:e.jsxs("div",{children:[e.jsx("span",{children:"Rain"}),e.jsx(s,{direction:"vertical"}),e.jsx("span",{children:"Home"}),e.jsx(s,{direction:"vertical","border-style":"dashed"}),e.jsx("span",{children:"Grass"})]})})}function i(r){const n={h1:"h1",h2:"h2",h3:"h3",p:"p",table:"table",tbody:"tbody",td:"td",th:"th",thead:"thead",tr:"tr",...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.h1,{children:"Divider 分割线"}),`
+`,`
+`,e.jsx(n.p,{children:"区隔内容的分割线。"}),`
+`,e.jsx(n.h2,{children:"基础用法"}),`
+`,e.jsx(n.p,{children:"对不同段落的文本进行分割。"}),`
+`,e.jsx(t,{name:"DividerBasic",children:e.jsx(h,{})}),`
+`,e.jsx(n.h2,{children:"设置文案"}),`
+`,e.jsx(n.p,{children:"可以在分割线上自定义文本内容。"}),`
+`,e.jsx(t,{name:"DividerContent",children:e.jsx(l,{})}),`
+`,e.jsx(n.h2,{children:"虚线"}),`
+`,e.jsx(n.p,{children:"您可以设置分隔符的样式。"}),`
+`,e.jsx(t,{name:"DividerBorder",children:e.jsx(o,{})}),`
+`,e.jsx(n.h2,{children:"垂直分隔线"}),`
+`,e.jsx(t,{name:"DividerDirection",children:e.jsx(c,{})}),`
+`,e.jsx(n.h2,{children:"API"}),`
+`,e.jsx(n.h3,{children:"Attributes"}),`
+`,e.jsxs(n.table,{children:[e.jsx(n.thead,{children:e.jsxs(n.tr,{children:[e.jsx(n.th,{children:"属性名"}),e.jsx(n.th,{children:"说明"}),e.jsx(n.th,{children:"类型"}),e.jsx(n.th,{children:"默认值"})]})}),e.jsxs(n.tbody,{children:[e.jsxs(n.tr,{children:[e.jsx(n.td,{children:"direction"}),e.jsx(n.td,{children:"设置分割线方向"}),e.jsx(n.td,{children:"'horizontal' / 'vertical'"}),e.jsx(n.td,{children:"horizontal"})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:"border-style"}),e.jsx(n.td,{children:"设置分隔符样式"}),e.jsx(n.td,{children:"'none' / 'solid' / 'hidden' / 'dashed' / ...(css/border-style)"}),e.jsx(n.td,{children:"solid"})]}),e.jsxs(n.tr,{children:[e.jsx(n.td,{children:"content-position"}),e.jsx(n.td,{children:"自定义分隔线内容的位置"}),e.jsx(n.td,{children:"'left' / 'right' / 'center'"}),e.jsx(n.td,{children:"center"})]})]})]})]})}function m(r={}){const{wrapper:n}=r.components||{};return n?e.jsx(n,{...r,children:e.jsx(i,{...r})}):i(r)}export{m as default};
